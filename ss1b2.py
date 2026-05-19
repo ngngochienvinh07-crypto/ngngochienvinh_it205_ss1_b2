ee178ab7b0466eb42fc585e1f40c1b3c6351ef01
ngngochienvinh07-crypto <ngngochienvinh07@gmail.com>
2026-05-19 14:36:51 +0700

    bt

---
+++ ss1b2.py
@@ -0,0 +1,23 @@
+# print (" --- HỆ THỐNG NHẬP CHI SỐ SINH TON -- ")
+# name_patient = input ("Nhập tên bệnh nhân : ")
+# weight = input ("Nhập cân nặng bệnh nhân : ")   #lỗi kiểu dữ liệu, hiện đang là kiểu chuỗi 
+
+# print(" --- KIỂM TRA DỮ LIỆU LƯU TRỮ --- ")
+# print ("Bệnh nhân : " , name_patient)
+# print ("Cân nặng dã nhập : ", weight)
+
+# # Trường nhóm IT viết thêm dòng này dể kiểm tra dữ liệu của cân nặng
+# print ("CẢNH BÁO - Kiểu dữ Liệu dang lưu là : ", type(weight))
+
+
+#Sửa
+print (" --- HỆ THỐNG NHẬP CHI SỐ SINH TON -- ")
+name_patient = input ("Nhập tên bệnh nhân : ")
+weight = int(input ("Nhập cân nặng bệnh nhân : ") )
+
+print(" --- KIỂM TRA DỮ LIỆU LƯU TRỮ --- ")
+print ("Bệnh nhân : " , name_patient)
+print ("Cân nặng đã nhập : ", weight)
+
+# Trường nhóm IT viết thêm dòng này dể kiểm tra dữ liệu của cân nặng
+print ("CẢNH BÁO - Kiểu dữ Liệu dang lưu là : ", type(weight))
